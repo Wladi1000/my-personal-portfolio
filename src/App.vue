e167050d6a09818386a9a9e3d58c792274f6cb97
Wladimir Sanvicente <wladimirss14@gmail.com>
2023-07-05 01:35:52 -0400

    Nueva estructura del portafolio

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <script setup>
+import navbar from "./components/navbar.vue";
 </script>
-
 <template>
-  <header>
-  <p>Esto es un header</p>
-  </header>
+  <navbar />
 
   <main>
-  <h1>Hola mundo</h1>
+    <h1>Hola mundo</h1>
+    <button class="btn btn-outline-danger">Boton bootstrap</button>
   </main>
 </template>
