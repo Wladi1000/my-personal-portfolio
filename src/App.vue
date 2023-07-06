79548adb3503bc3a1575acfd6737f0e192785337
Wladimir Sanvicente <wladimirss14@gmail.com>
2023-07-06 16:39:27 -0400

    Sumando seccion de experiencia

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import navbar from "./components/navbar.vue";
 import homeAboutMeVue from "./sections/home-about-me.vue";
+import homeExperienceVue from "./sections/home-experience.vue";
 import homeHeroVue from "./sections/home-hero.vue";
 </script>
 <template>
   <navbar />
   <homeHeroVue />
   <homeAboutMeVue/>
+  <homeExperienceVue/>
 </template>
