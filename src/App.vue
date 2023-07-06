e8726d2794db993b379fd32bfce56d2418e2f4a7
Wladimir Sanvicente <wladimirss14@gmail.com>
2023-07-06 03:16:28 -0400

    Sumando about me

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import navbar from "./components/navbar.vue";
+import homeAboutMeVue from "./sections/home-about-me.vue";
 import homeHeroVue from "./sections/home-hero.vue";
 </script>
 <template>
   <navbar />
   <homeHeroVue />
+  <homeAboutMeVue/>
 </template>
--- src/sections/home-about-me.vue
+++ src/sections/home-about-me.vue
@@ -1,5 +1,10 @@
 <script setup>
 </script>
 <template>
-    
+    <section class="about-me section-oscura">
+        <div class="contenedor">
+            <h2 class="section-titulo texto-blanco">Meet Wladimir</h2>
+            <p class="section-texto">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque laudantium eveniet totam ab nulla asperiores sequi tempore et adipisci architecto quibusdam deserunt, soluta exercitationem animi numquam voluptate quos libero labore.</p>
+        </div>
+    </section>
 </template>
