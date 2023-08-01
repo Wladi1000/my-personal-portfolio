6d26044ce65abe2a564e3e092597f17cd0cdb37f
Wladimir Sanvicente <wladimirss14@gmail.com>
2023-07-31 20:59:31 -0400

    Actualizacion de experiencias

--- src/sections/home-experience.vue
+++ src/sections/home-experience.vue
@@ -8,7 +8,7 @@
         <div class="column col-12 col-md-4">
             <i class="bi bi-laptop"></i>
             <p class="experience-title">Web Develoment</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, cum necessitatibus doloribus exs sapiente voluptate quam neque dignissimos dolore unde vero!</p>
+            <p>Front-End development in Pasastias at UCAB Extension Guayana.</p>
             <div class="badges-contenedor">
                 <span class="badge bg-primary">HTML</span>
                 <span class="badge bg-primary">CSS</span>
@@ -20,9 +20,9 @@
         </div>
         <!-- Juegos -->
         <div class="column col-12 col-md-4">
-            <i class="bi bi-laptop"></i>
+            <i class="bi bi-chat-dots"></i>
             <p class="experience-title">Articles</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, cum necessitatibus doloribus exs sapiente voluptate quam neque dignissimos dolore unde vero!</p>
+            <p>Contributed on topics of common interest on servers dedicated to "Sky: Children of the Light".</p>
             <div class="badges-contenedor">
                 <span class="badge bg-primary">Writting</span>
                 <span class="badge bg-primary">Edit</span>
@@ -31,17 +31,15 @@
         </div>
         <!-- Estudiante -->
         <div class="column col-12 col-md-4">
-            <i class="bi bi-laptop"></i>
+            <i class="bi bi-journal-bookmark"></i>
             <p class="experience-title">Studies</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, cum necessitatibus doloribus exs sapiente voluptate quam neque dignissimos dolore unde vero!</p>
+            <p>Computer Engineering studies at UCAB Extension Guayana having completed all the subjects of the career, also having participated in several projects required to pass the subjects.</p>
             <div class="badges-contenedor">
                 <span class="badge bg-primary">Computer Science </span>
                 <span class="badge bg-primary">Web Develoment</span>
                 <span class="badge bg-primary">Software Develoment</span>
                 <span class="badge bg-primary">Data Base Develoment</span>
-                <span class="badge bg-primary">Pythont</span>
-                <span class="badge bg-primary">Node.js</span>
-                <span class="badge bg-primary">Express</span>
+                <span class="badge bg-primary">IoT Development</span>
             </div>
         </div>
       </div>
